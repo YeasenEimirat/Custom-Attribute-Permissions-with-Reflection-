db01a8486ea850089e8a6aa572882da1a4165889
Yaseen Eimirat <yaseeneimirat23@gmail.com>
2026-08-28 19:03:40 +0300

    Rename project and update documentation

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -1,37 +1,37 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace ConsoleApp7
 {
+    // Custom Attribute used to define
+    // the permission required by a method.
     [AttributeUsage(AttributeTargets.Method)]
     public class RequirePermissionAttribute : Attribute
     {
         public string Permission { get; }
+
         public RequirePermissionAttribute(string permission)
         {
             Permission = permission;
         }
     }
+
     public class UserService
     {
         [RequirePermission("Users.Create")]
-
-
         public void CreateUser()
         {
             Console.WriteLine("User Created");
         }
-        [RequirePermission("Users.Delete")]
 
+        [RequirePermission("Users.Delete")]
         public void DeleteUser()
         {
             Console.WriteLine("User Deleted");
         }
-
 
         [RequirePermission("Users.View")]
         public void ViewUsers()
@@ -43,22 +43,57 @@ namespace ConsoleApp7
     public class User
     {
         public string Name { get; set; }
+
         public List<string> Permissions { get; set; }
     }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            // Create a user with specific permissions.
             User user = new User
             {
                 Name = "Yaseen",
-                Permissions = new List<string> {  "Users.Create",  "Users.View" } };
-            Type type = typeof(UserService);
-            MethodInfo method = type.GetMethod("DeleteUser");
-            RequirePermissionAttribute attribute =
-                method.GetCustomAttribute<RequirePermissionAttribute>();
+                Permissions = new List<string>
+                {
+                    "Users.Create",
+                    "Users.View"
+                }
+            };
 
-            Console.WriteLine(attribute.Permission);
+            // Get the UserService type using Reflection.
+            Type type = typeof(UserService);
+
+            // Get the DeleteUser method using Reflection.
+            MethodInfo method = type.GetMethod("DeleteUser");
+
+            // Create an instance of UserService.
+            UserService service = new UserService();
+
+            // Read the Custom Attribute from the method.
+            RequirePermissionAttribute attribute =
+                method?.GetCustomAttribute<RequirePermissionAttribute>();
+
+            // If the method does not require a permission,
+            // execute it directly.
+            if (attribute == null)
+            {
+                method?.Invoke(service, null);
+            }
+            // Check if the current user has the required permission.
+            else if (user.Permissions.Contains(attribute.Permission))
+            {
+                Console.WriteLine("Access Granted");
+
+                // Execute the method dynamically using Reflection.
+                method.Invoke(service, null);
+            }
+            else
+            {
+                Console.WriteLine("Access Denied");
+            }
+
             Console.ReadKey();
         }
     }
